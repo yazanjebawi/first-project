5ee21032b4346cd4df5212324dbf3760fad8209e
eslam-obeida <jebawiyazan@gmail.com>
2024-06-23 03:43:32 -0700

    js

---
+++ js/ibdex.js
@@ -0,0 +1,27 @@
+console.log('hdkdk')
+var c='thes is new var'
+console.log(c)
+c=10
+console.log(c)
+let d ='thes is d'
+console.log(d)
+const b ='thes is b'
+console.log(b)
+var x=null
+console.log(x)
+var boolNew =true
+console.log(boolNew)
+var list1=[1,2,3,4,5,6,7,8,9]
+list1[5]=10
+console.log(list1)
+var obj1={name:'yazan',age:25}
+console.log(obj1['age'])
+console.log(obj1.name)
+m=2
+if(m>2){
+    console.log('m is greater than>2')
+
+}
+else{
+    console.log('m is not greater than 2')
+}
